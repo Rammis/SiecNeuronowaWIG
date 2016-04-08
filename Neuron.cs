d955742f31ae399da7a4ceabec36da7a7221b8eb
Michal Kaluzinski <rammis94@gmail.com>
2016-04-08 13:55:17 +0200

    Pierwszy commit (odczytywanie danych z plików, tworzenie neurona i nadawanie mu losowych wag z przedziału <0.01;1>

---
+++ Neuron.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiecNeuronowa
+{
+    class Neuron
+    {
+        List<Double> wagi = new List<Double>();
+        List<Double> wejscia = new List<Double>();
+
+        public Neuron(int iloscWejsc)
+        {
+            
+            
+            generateRandomWeights(iloscWejsc);
+
+        }
+
+        public void setValues(List<Double> _wejscia){
+
+            for (int i = 0; i < _wejscia.Count; i++)
+            {
+                wejscia.Add(_wejscia[i]);
+            }
+        }
+
+        private void generateRandomWeights(int iloscWejsc)
+            {
+
+                Random r = new Random();
+
+                for (int i = 0; i < iloscWejsc; i++)
+                {
+                    double tmp = r.NextDouble() * (1 - 0.01) + 0.01;
+                    wagi.Add(tmp);
+                }
+            }
+
+        public List<Double> getWeights()
+        {
+            return wagi;
+        }
+
+        public List<Double> getValues()
+        {
+            return wejscia;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiecNeuronowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"Wig.txt");
+            List<double> liczby = new List<double>();
+
+
+            Neuron neuron = new Neuron(5);
+            
+            for(int i =0; i< lines.Length; i++)
+                liczby.Add(double.Parse(lines[i]));
+
+            neuron.setValues(liczby);
+
+            List<Double> _wagi = neuron.getWeights();
+            List<Double> _wejscia = neuron.getValues();
+            for (int i = 0; i < _wagi.Count; i++)
+            {
+                Console.Write(_wagi[i]);
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < _wejscia.Count; i++)
+            {
+                Console.Write(_wejscia[i]);
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
